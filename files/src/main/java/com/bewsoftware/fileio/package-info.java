468f5a3ad1399a93d7da48855cccc8dbe4f2d557
Bradley Willcott <bw.opensource@yahoo.com>
2021-05-09 16:45:14 +0800

    Updates license header. Signed-off-by:Bradley Willcott <bw.opensource@yahoo.com>

--- files/src/main/java/com/bewsoftware/fileio/package-info.java
+++ files/src/main/java/com/bewsoftware/fileio/package-info.java
@@ -1,20 +1,21 @@
 /*
- * This file is part of the BEW Files Library (aka: BEWFiles).
+ *  File Name:    package-info.java
+ *  Project Name: bewsoftware-files
  *
- * Copyright (C) 2020 Bradley Willcott
+ *  Copyright (c) 2020 Bradley Willcott
  *
- * BEWFiles is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
  *
- * BEWFiles is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 /**
  * To provide classes related to I/O and types of files.
