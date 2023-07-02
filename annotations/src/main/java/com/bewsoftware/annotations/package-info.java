c41e5a172a0b6547603930c8d7b2ab5da720fd8d
Bradley Willcott <bw.opensource@yahoo.com>
2023-07-02 15:35:05 +0800

    Initial commit for Annotations library.

---
+++ annotations/src/main/java/com/bewsoftware/annotations/package-info.java
@@ -0,0 +1,30 @@
+/*
+ *  File Name:    package-info.java
+ *  Project Name: bewsoftware.annotations
+ *
+ *  Copyright (c) 2023 Bradley Willcott
+ *
+ *  bewsoftware.annotations is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  bewsoftware.annotations is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * All of the interfaces in this package were copied out of the Maven plugin:
+ * <code><b>net.jcip:jcip-annotations:1.0</b></code>.
+ *
+ * @author <a href="mailto:bw.opensource@yahoo.com">Bradley Willcott</a>
+ *
+ * @since 3.0.0
+ * @version 3.0.0
+ */
+package com.bewsoftware.annotations;
