73567f10c265a2630ed0e60502c5c7f673c804c2
Bradley Willcott <bw.opensource@yahoo.com>
2023-07-02 16:44:39 +0800

    Updated file in modular project.

--- common/src/main/java/com/bewsoftware/common/package-info.java
+++ common/src/main/java/com/bewsoftware/common/package-info.java
@@ -1,28 +1,30 @@
 /*
- * This file is part of the BEWSoftware Common Library (aka: BEWCommon).
- *
- * Copyright (C) 2020 Bradley Willcott
- *
- * BEWCommon is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * BEWCommon is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  File Name:    package-info.java
+ *  Project Name: bewsoftware-common
+ * 
+ *  Copyright (c) 2023 Bradley Willcott
+ * 
+ *  bewsoftware-common is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  bewsoftware-common is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 /**
- * These classes are of a general nature, and include sub-classes of {@code Exception}
+ * These classes are of a general nature, and include sub-classes of {@code Thrown}
  * that are used by classes from various modules.
- *
- * @author <a href="mailto:bw.opensource@yahoo.com">Bradley Willcott</a>
- *
- * @since 1.0
- * @version 1.0
- */
+*
+* @author <a href="mailto:bw.opensource@yahoo.com">Bradley Willcott</a>
+*
+* @since 1.0.0
+* @version 3.0.0
+*/
 package com.bewsoftware.common;
